b766fdc606e43f4ebcb3099059ca4cc63a2ed0a0
Igor Flores <81481911+IgorFlores2003@users.noreply.github.com>
2023-10-26 13:09:43 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <button>
+  Filminho
+</button>
+
+<button>
+  Surpresa
+</button>
+
+<button>
+  Sonho
+</button>
+</header>
+      <div>
+      <p>Texto aqui</p>
+      </div>
+      <div>
+<h1>Planejando fazer uma linha do tempo aqui!!</h1>
+      </div>
+    </div>
+  
+  );
+}
+
+export default App;
